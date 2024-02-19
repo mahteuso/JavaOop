938d311ce099a38a1fa124e778afe8fd206bbdcb
Mateus <mateus@ufscar.br>
2024-02-19 11:44:02 -0300

    usando JoptionPane

--- untitled/src/cursojava/executavel/FirstClassJava.java
+++ untitled/src/cursojava/executavel/FirstClassJava.java
@@ -2,20 +2,28 @@ package cursojava.executavel;
 
 import cursojava.classes.Aluno;
 
+import javax.swing.*;
+
 public class FirstClassJava {
     public static void main(String[] args) {
-        Aluno mateus = new Aluno("Mateus", " Laranjeira");
-        System.out.println(mateus.getName());
-        mateus.setName("Mateus", " Silva");
-        System.out.println(mateus.getName());
-        mateus.setAge(43);
-        mateus.setcpfNumber("298.383.528-61");
+
+        String firstName = JOptionPane.showInputDialog("Entre com o primeiro nome do aluno");
+        String lastName = JOptionPane.showInputDialog("Entre com o ultimo nome");
+        String age = JOptionPane.showInputDialog("Entre com a idade do aluno");
+        String CPF = JOptionPane.showInputDialog("Entre com o cpf do aluno");
+        Aluno mateus = new Aluno(firstName, lastName);
+        mateus.setAge(Integer.parseInt(age));
+        mateus.setcpfNumber(CPF);
         System.out.println("Name: " + mateus.getName() + " Age: " + mateus.getAge() + " CPF: " + mateus.getCpfNumber());
 
-        mateus.setNota1(10);
-        mateus.setNota2(5.7);
-        mateus.setNota3(8.2);
-        mateus.setNota4(7.1);
+        String nota1 = JOptionPane.showInputDialog("Entre com a nota da primeira prova:");
+        String nota2 = JOptionPane.showInputDialog("Entre com a nota da segunda prova:");
+        String nota3 = JOptionPane.showInputDialog("Entre com a nota da terceira prova:");
+        String nota4 = JOptionPane.showInputDialog("Entre com a nota da quarta prova:");
+        mateus.setNota1(Double.parseDouble(nota1));
+        mateus.setNota2(Double.parseDouble(nota2));
+        mateus.setNota3(Double.parseDouble(nota3));
+        mateus.setNota4(Double.parseDouble(nota4));
         System.out.println("O aluno: " + mateus.getName() + " recebeu uma média de: " + mateus.getMediaNota() + mateus.getAprovacao());
     }
 }
