325fb1808b64072f5a8f211f3785baef5ecaa5b5
Mateus <mateus@ufscar.br>
2024-02-19 10:33:20 -0300

    Criados os métodos get and setters.

--- untitled/src/cursojava/classes/Aluno.java
+++ untitled/src/cursojava/classes/Aluno.java
@@ -1,13 +1,86 @@
 package cursojava.classes;
 
 public class Aluno {
-    public String name;
-    public int age;
-    public String dateBorn;
-    public String rg;
-    public String cpfNumber;
+    private String name;
+    private int age;
+    private String cpfNumber;
 
-    public Aluno(String firstName, String lastName){
-        name = firstName+lastName;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double nota4;
+
+
+    public Aluno(String firstName, String lastName) {
+        name = firstName + lastName;
+    }
+
+    public void setName(String firstName, String lastName) {
+        this.name = firstName + lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setcpfNumber(String cpfNumber) {
+        this.cpfNumber = cpfNumber;
+    }
+
+    public String getCpfNumber() {
+        return cpfNumber;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+
+    public double getMediaNota() {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+
+    public String getAprovacao(){
+        if (getMediaNota() > 5){
+            return " Aluno aprovado";
+        }
+        else{
+            return " Aluno reprovado";
+        }
     }
 }
--- untitled/src/cursojava/executavel/FirstClassJava.java
+++ untitled/src/cursojava/executavel/FirstClassJava.java
@@ -3,9 +3,19 @@ package cursojava.executavel;
 import cursojava.classes.Aluno;
 
 public class FirstClassJava {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Aluno mateus = new Aluno("Mateus", " Laranjeira");
-        System.out.println(mateus.name);
+        System.out.println(mateus.getName());
+        mateus.setName("Mateus", " Silva");
+        System.out.println(mateus.getName());
+        mateus.setAge(43);
+        mateus.setcpfNumber("298.383.528-61");
+        System.out.println("Name: " + mateus.getName() + " Age: " + mateus.getAge() + " CPF: " + mateus.getCpfNumber());
 
+        mateus.setNota1(10);
+        mateus.setNota2(5.7);
+        mateus.setNota3(8.2);
+        mateus.setNota4(7.1);
+        System.out.println("O aluno: " + mateus.getName() + " recebeu uma média de: " + mateus.getMediaNota() + mateus.getAprovacao());
     }
 }
