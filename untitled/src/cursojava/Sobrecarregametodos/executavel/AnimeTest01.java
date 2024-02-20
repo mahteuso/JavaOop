d538eb5d10ae5cc9af23767763e3f7194ac7e224
Mateus <mateus@ufscar.br>
2024-02-20 14:17:56 -0300

    usando SobrecargadeMetodos

---
+++ untitled/src/cursojava/Sobrecarregametodos/executavel/AnimeTest01.java
@@ -0,0 +1,13 @@
+package cursojava.Sobrecarregametodos.executavel;
+
+import cursojava.Sobrecarregametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto", "Shounen", 39);
+
+        System.out.println("Anime type: "+anime.getType()+"\nEpisodes: "+anime.getEpisode());
+
+        System.out.println(anime.toString());
+    }
+}
