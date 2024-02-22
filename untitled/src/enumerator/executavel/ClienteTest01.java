a4d6fcaaa2c9ea997456a0c22a99563f0086f71c
Mateus <mateus@ufscar.br>
2024-02-22 12:14:43 -0300

    Config: Atualizando o teste

--- untitled/src/enumerator/executavel/ClienteTest01.java
+++ untitled/src/enumerator/executavel/ClienteTest01.java
@@ -1,7 +1,18 @@
 package enumerator.executavel;
 
+import enumerator.domain.Cliente;
+import enumerator.domain.TipoCliente;
+import enumerator.domain.Cliente.TipoPagamento;
+
+import java.lang.ref.Cleaner;
+
 public class ClienteTest01 {
     public static void main(String[] args) {
+        Cliente cliente01 = new Cliente("Mateus", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente02 = new Cliente("Marco", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
+        System.out.println(cliente01);
+        System.out.println(cliente02);
 
     }
 }
