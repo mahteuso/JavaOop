a04cc1303f1cddd485273e2b74d0ab6bfbbed22a
Mateus <mateus@ufscar.br>
2024-02-22 12:33:47 -0300

    Config: Adicionando um método abstrato para calcular o desconto do produto

--- untitled/src/enumerator/domain/Cliente.java
+++ untitled/src/enumerator/domain/Cliente.java
@@ -2,7 +2,26 @@ package enumerator.domain;
 
 public class Cliente {
     public enum TipoPagamento{
-        DEBITO, CREDITO
+        DEBITO('D'){
+            @Override
+            public double calcularDesconto(double valor) {
+                return valor * 0.9;
+            }
+        },
+        CREDITO('C'){
+            @Override
+            public double calcularDesconto(double valor) {
+                return valor * 1.1;
+            }
+        };
+
+        public final char VALOR;
+
+        TipoPagamento(char valor){
+            this.VALOR = valor;
+        }
+        public abstract double calcularDesconto(double valor);
+
     }
     private String nome;
     private TipoCliente tipoCliente;
@@ -14,12 +33,15 @@ public class Cliente {
         this.tipoPagamento = tipoPagamento;
     }
 
+
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
                 ", tipoCliente=" + tipoCliente +
+                ", tipoCliente=" + tipoCliente.VALOR +
                 ", tipoPagamento=" + tipoPagamento +
+                ", tipoPagamento=" + tipoPagamento.VALOR +
                 '}';
     }
 }
