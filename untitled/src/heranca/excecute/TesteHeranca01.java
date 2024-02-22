3acc9792911f33745a6d46aee2628f3c563b2ab2
Mateus <mateus@ufscar.br>
2024-02-22 09:38:06 -0300

    Config: Atualizando o arquivo executável

--- untitled/src/heranca/excecute/TesteHeranca01.java
+++ untitled/src/heranca/excecute/TesteHeranca01.java
@@ -6,20 +6,17 @@ import heranca.domain.Pessoa;
 
 public class TesteHeranca01 {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        Funcionario funcionario = new Funcionario();
+        Pessoa pessoa = new Pessoa("Mateus");
+        Funcionario funcionario = new Funcionario("Mateus");
         Endereco endereco = new Endereco();
 
         endereco.setRua("Rua dos Sabiás");
-
-        pessoa.setNome("Mateus");
         pessoa.setCpf("123456678-90");
         pessoa.setEndereco(endereco);
 
         pessoa.Imprimir();
         System.out.println("----------------");
 
-        funcionario.setNome("Mateus");
         funcionario.setCpf("123456678-90");
         funcionario.setEndereco(endereco);
         funcionario.setSalario("R$ 2000.00");
