14bf88df8a080989a76e480dc434cdf55f3d0f50
Mateus <mateus@ufscar.br>
2024-02-26 12:20:40 -0300

    Atualizando

--- untitled/src/enumerator/domain/TipoCliente.java
+++ untitled/src/enumerator/domain/TipoCliente.java
@@ -1,12 +1,14 @@
 package enumerator.domain;
 
 public enum TipoCliente {
-    PESSOA_FISICA(1),
-    PESSOA_JURIDICA(2);
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
 
     public final int VALOR;
-
-    TipoCliente(int valor){
+    private String nomeRelatorio;
+    TipoCliente(int valor, String nomeRelatorio){
         this.VALOR = valor;
+        this.nomeRelatorio = nomeRelatorio;
     }
+
 }
