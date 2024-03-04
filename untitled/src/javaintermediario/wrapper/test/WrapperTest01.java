a2ed7d0d3b731228b70e5e3ce3d12b8e9329aced
Mateus <mateus@ufscar.br>
2024-03-04 12:30:14 -0300

    Wrapper teste01

---
+++ untitled/src/javaintermediario/wrapper/test/WrapperTest01.java
@@ -0,0 +1,13 @@
+package javaintermediario.wrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        Integer intW = 10; // autoboxing
+
+        int i = intW; // unboxing
+
+        boolean falso = Boolean.parseBoolean("False");
+        System.out.println(falso);
+
+    }
+}
