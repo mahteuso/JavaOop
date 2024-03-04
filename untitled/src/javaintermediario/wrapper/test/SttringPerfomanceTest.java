df16f77d411eddb44ef8b52ce742c0757e221370
Mateus <mateus@ufscar.br>
2024-03-04 15:03:37 -0300

    Estudo de Strings

---
+++ untitled/src/javaintermediario/wrapper/test/SttringPerfomanceTest.java
@@ -0,0 +1,37 @@
+package javaintermediario.wrapper.test;
+
+public class SttringPerfomanceTest {
+    public static void main(String[] args) {
+        double inicio = System.currentTimeMillis();
+        concatString(100_000);
+        double fim = System.currentTimeMillis();
+        double total = fim - inicio;
+
+        System.out.println("Tempo gasto concatenando Strings: git add ."+total+ " ms");
+        System.out.println("------------------");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(100_000);
+        fim = System.currentTimeMillis();
+        total = fim - inicio;
+
+        System.out.println("Tempo gasto com StringBuilder: "+total+ " ms");
+        System.out.println("------------------");
+    }
+
+    public static void concatString(int tamanho){
+        String test = "";
+        for (int i = 0; i < tamanho; i++){
+            test += i;
+        }
+
+    }
+
+    public static void concatStringBuilder(int tamanho){
+        StringBuilder test = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++){
+            test.append(i);
+        }
+
+    }
+}
